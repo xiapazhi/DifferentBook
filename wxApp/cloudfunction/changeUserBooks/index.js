8a79e368708f30ac6930805b68a6660ee4ee1b10
YUAN <1650192445@qq.com>
2019-08-19 17:45:03 +0800

    提交书单

--- wxApp/cloudfunction/changeUserBooks/index.js
+++ wxApp/cloudfunction/changeUserBooks/index.js
@@ -4,15 +4,33 @@ const cloud = require('wx-server-sdk')
 cloud.init()
 
 // 云函数入口函数
-exports.main = async (event, context) => {
-   const wxContext = cloud.getWXContext()
+exports.main = async(event, context) => {
+  const db = cloud.database()
+  const wxContext = cloud.getWXContext()
 
-   
+  const booksList = event.booksList;
+  for (let v of booksList) {
+    if(v.bookName && !v.bookAuthor){
+      let currBookRes = db.collection("books").where({
+        name:v.bookName
+      }).limit(1).get()
+    }
+    
+  }
+  let addRes = await db.collection("books").add({
+    data: {
+      name: booksList[0].bookName,
+      author: booksList[0].bookAuthor,
+      collect_nums: 0
+    }
+  })
 
-   return {
-      event,
-      openid: wxContext.OPENID,
-      appid: wxContext.APPID,
-      unionid: wxContext.UNIONID,
-   }
+  return {
+    addRes,
+    event,
+    openid: wxContext.OPENID,
+    appid: wxContext.APPID,
+    unionid: wxContext.UNIONID,
+  }
+
 }
--- wxApp/pages/index/index.js
+++ wxApp/pages/index/index.js
@@ -3,171 +3,183 @@
 const app = getApp()
 
 Page({
-   data: {
-      scopeUserInfo: false,
-      userInfo: null,
-      canIUse: wx.canIUse('button.open-type.getUserInfo'),
+  data: {
+    scopeUserInfo: false,
+    userInfo: null,
+    canIUse: wx.canIUse('button.open-type.getUserInfo'),
 
-      aniSettingsRotate: null,
-      showModal: false,
-      currModalContent: 'editBookList', //'settings',
-      currModalTitle: '编辑书单', //'设置',
-      windowHeight: 600,
-      booksList: [{
-         bookName: '',
-         bookAuthor: ''
-      }],
-   },
+    aniSettingsRotate: null,
+    showModal: true,
+    currModalContent: 'editBookList', //'settings',
+    currModalTitle: '编辑书单', //'设置',
+    windowHeight: 600,
+    booksList: [{
+      bookName: '',
+      bookAuthor: ''
+    }],
+  },
 
-   getUserInfo: function(e) {
-      app.globalData.userInfo = e.detail.userInfo
-      this.setData({
-         userInfo: e.detail.userInfo,
-         scopeUserInfo: true
-      })
-   },
+  getUserInfo: function(e) {
+    app.globalData.userInfo = e.detail.userInfo
+    this.setData({
+      userInfo: e.detail.userInfo,
+      scopeUserInfo: true
+    })
+  },
 
-   showModal() {
-      this.setData({
-         showModal: true
-      })
-   },
+  showModal() {
+    this.setData({
+      showModal: true
+    })
+  },
 
-   hideModal() {
-      this.setData({
-         showModal: false
-      })
-   },
+  hideModal() {
+    this.setData({
+      showModal: false
+    })
+  },
 
-   submitBookList(e) {
-      console.log(e)
-      const {
-         booksList
-      } = this.data;
-      console.log(booksList);
-      // 调用云函数
-      wx.cloud.callFunction({
-         name: 'changeUserBooks',
-         data: {
-            booksList: booksList
-         },
-         success: res => {
-            console.log(res)
-         },
-         fail: err => {
-            console.log(err)
-         }
-      })
-   },
-
-   addBook() {
-      let {
-         booksList
-      } = this.data;
-      booksList.push({
-         bookName: '',
-         bookAuthor: ''
-      });
-      this.setData({
-         booksList: booksList
-      })
-   },
-
-   delBook(e) {
-      console.log(e)
-      let {
-         booksList
-      } = this.data;
-      let index = e.currentTarget.dataset.index;
-      booksList.splice(index, 1);
-      this.setData({
-         booksList: booksList
-      })
-   },
-
-   bookMsgInput(e) {
-      console.log(e)
-      let {
-         booksList
-      } = this.data;
-      let dataSet = e.currentTarget.dataset;
-      let value = e.detail.value;
-      if (dataSet.type == 'name') {
-         booksList[dataSet.index].bookName = value;
-      } else {
-         booksList[dataSet.index].bookAuthor = value;
+  submitBookList(e) {
+    console.log(e)
+    const {
+      booksList
+    } = this.data;
+    console.log(booksList);
+    let confirmBooksList = [];
+    for (let i in booksList) {
+      if (confirmBooksList.some(c => c.bookName == booksList[i].bookName)) {
+        return wx.showToast({
+          title: `序号为${parseInt(i) + 1}的作品《${booksList[i].bookName}》重复啦`,
+          icon: 'none',
+        })
       }
-   },
+      if (booksList[i].bookName) {
+        confirmBooksList.push(booksList[i])
+      }
+    }
+    // 调用云函数
+    wx.cloud.callFunction({
+      name: 'changeUserBooks',
+      data: {
+        booksList: confirmBooksList
+      },
+      success: res => {
+        console.log(res)
+      },
+      fail: err => {
+        console.log(err)
+      }
+    })
+  },
 
-   /**
-    * 生命周期函数--监听页面加载
-    */
-   onLoad: function() {
-      wx.getUserInfo({
-         success: res => {
-            app.globalData.userInfo = res.userInfo
-            this.setData({
-               userInfo: res.userInfo,
-               scopeUserInfo: true
-            })
-         }
-      })
-   },
+  addBook() {
+    let {
+      booksList
+    } = this.data;
+    booksList.push({
+      bookName: '',
+      bookAuthor: ''
+    });
+    this.setData({
+      booksList: booksList
+    })
+  },
 
-   /**
-    * 生命周期函数--监听页面初次渲染完成
-    */
-   onReady: function() {
-      const that = this;
-      wx.getSystemInfo({
-         success(res) {
-            console.log(res.windowHeight)
-            that.setData({
-               windowHeight: res.windowHeight
-            })
-         }
-      })
-   },
+  delBook(e) {
+    console.log(e)
+    let {
+      booksList
+    } = this.data;
+    let index = e.currentTarget.dataset.index;
+    booksList.splice(index, 1);
+    this.setData({
+      booksList: booksList
+    })
+  },
 
-   /**
-    * 生命周期函数--监听页面显示
-    */
-   onShow: function() {
+  bookMsgInput(e) {
+    console.log(e)
+    let {
+      booksList
+    } = this.data;
+    let dataSet = e.currentTarget.dataset;
+    let value = e.detail.value.replace(/\s*/g, "");
+    if (dataSet.type == 'name') {
+      booksList[dataSet.index].bookName = value;
+    } else {
+      booksList[dataSet.index].bookAuthor = value;
+    }
+  },
 
-   },
+  /**
+   * 生命周期函数--监听页面加载
+   */
+  onLoad: function() {
+    wx.getUserInfo({
+      success: res => {
+        app.globalData.userInfo = res.userInfo
+        this.setData({
+          userInfo: res.userInfo,
+          scopeUserInfo: true
+        })
+      }
+    })
+  },
 
-   /**
-    * 生命周期函数--监听页面隐藏
-    */
-   onHide: function() {
+  /**
+   * 生命周期函数--监听页面初次渲染完成
+   */
+  onReady: function() {
+    const that = this;
+    wx.getSystemInfo({
+      success(res) {
+        console.log(res.windowHeight)
+        that.setData({
+          windowHeight: res.windowHeight
+        })
+      }
+    })
+  },
 
-   },
+  /**
+   * 生命周期函数--监听页面显示
+   */
+  onShow: function() {
 
-   /**
-    * 生命周期函数--监听页面卸载
-    */
-   onUnload: function() {
+  },
 
-   },
+  /**
+   * 生命周期函数--监听页面隐藏
+   */
+  onHide: function() {
 
-   /**
-    * 页面相关事件处理函数--监听用户下拉动作
-    */
-   onPullDownRefresh: function() {
+  },
 
-   },
+  /**
+   * 生命周期函数--监听页面卸载
+   */
+  onUnload: function() {
 
-   /**
-    * 页面上拉触底事件的处理函数
-    */
-   onReachBottom: function() {
+  },
 
-   },
+  /**
+   * 页面相关事件处理函数--监听用户下拉动作
+   */
+  onPullDownRefresh: function() {
 
-   /**
-    * 用户点击右上角分享
-    */
-   onShareAppMessage: function() {
+  },
 
-   }
+  /**
+   * 页面上拉触底事件的处理函数
+   */
+  onReachBottom: function() {
+
+  },
+
+  /**
+   * 用户点击右上角分享
+   */
+  onShareAppMessage: function() {
+
+  }
 })
